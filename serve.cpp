c1ef8771762ae1cf5750184b2cbc11e4d87c9220
hackersean <hackersean@users.noreply.github.com>
2014-09-05 16:53:23 +0800

    lianjie

--- serve.cpp
+++ serve.cpp
@@ -4,33 +4,31 @@ using namespace std;
 char path[]="/data/data";
 
 
-void read_data(DATA &data)
+void read_data(DATA &data,int fd)
 {
 	int len;
 	NODE temp;
     while(fgets(temp.str,BUFFER,data.fp)!=NULL)
 	{
 	      len=temp.play();
-//		  serve.send(temp.ans,len);
-		  cout<<temp.ans;
+          send(fd,temp.ans,len,0);
+		  
 	}
-	
-	     
 }
 
 int main(int ac,char *av[])
 {
 	if(ac!=2)
 	{
-		//	oops("argument error");
+		oops("argument error");
 	} 
 	
-//	c_serve serve(atoi(av[1]));
+	c_serve serve(atoi(av[1]));
 
-//	serve.accept();
+	int fd=serve.accept();
 	 
 	DATA data(path); 
-	read_data(data);
+	read_data(data,fd);
 	
 
 	
