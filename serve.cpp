a295e0f3a8304f87b917eb650c87d1a27c359621
hackersean <hackersean@users.noreply.github.com>
2014-09-05 16:32:20 +0800

    xlon

--- serve.cpp
+++ serve.cpp
@@ -6,7 +6,6 @@ char path[]="/data/data";
 
 void read_data(DATA &data)
 {
-<<<<<<< HEAD
 	int len;
 	NODE temp;
     while(fgets(temp.str,BUFFER,data.fp)!=NULL)
@@ -15,13 +14,8 @@ void read_data(DATA &data)
 //		  serve.send(temp.ans,len);
 		  cout<<temp.ans;
 	}
-=======
-<<<<<<< HEAD
 	
-=======
 	     
->>>>>>> d8d585ead6c010090ade7986e5632ef0349440ea
->>>>>>> 4c0fe5afbd58a31d8512718c1bb2be06d7b06342
 }
 
 int main(int ac,char *av[])
